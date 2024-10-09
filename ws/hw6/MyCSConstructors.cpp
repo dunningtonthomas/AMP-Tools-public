180a34031c9ebc6c570fab5d54172b87dd00cdc5
Thomas Dunnington <dunningtonthomas@gmail.com>
2024-10-08 18:59:29 -0600

    Update

--- ws/hw6/MyCSConstructors.cpp
+++ ws/hw6/MyCSConstructors.cpp
@@ -34,12 +34,109 @@ std::unique_ptr<amp::GridCSpace2D> MyPointAgentCSConstructor::construct(const am
     return cspace_ptr;
 }
 
+
+// @brief Function to check if a point is in collision with an obstacle
+bool MyPointAgentCSConstructor::inCollision_point(const amp::Environment2D& env, const Eigen::Vector2d& point) {
+    // Loop through all the obstacles
+    for(const auto& obstacle : env.obstacles) {
+        // Check if the point is inside the obstacle
+        if(isInsidePolygon(obstacle, point)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 amp::Path2D MyWaveFrontAlgorithm::planInCSpace(const Eigen::Vector2d& q_init, const Eigen::Vector2d& q_goal, const amp::GridCSpace2D& grid_cspace, bool isManipulator) {
     // Implement your WaveFront algorithm here
     // This path should be in continuous space
     amp::Path2D path;
     path.waypoints.push_back(q_init);
     path.waypoints.push_back(q_goal);
+
+    // Get the number of cells in the C-space
+    std::pair<std::size_t, std::size_t> n_cells = grid_cspace.size();
+    int distanceArr[n_cells.first][n_cells.second];
+
+    // Loop through the C-space and assign 1 to all obstacles, 0 to all free cells, and 2 to the goal cell
+    for(std::size_t i = 0; i < n_cells.first; i++) {
+        for(std::size_t j = 0; j < n_cells.second; j++) {
+            if(grid_cspace(i, j)) {
+                distanceArr[i][j] = 1;
+            } else {
+                distanceArr[i][j] = 0;
+            }
+        }
+    }
+
+    // Set the goal cell to 2
+    std::pair<std::size_t, std::size_t> goal_cell = grid_cspace.getCellFromPoint(q_goal(0), q_goal(1));
+    distanceArr[goal_cell.first][goal_cell.second] = 2;
+
+    // Create a queue of cell locations
+    std::queue<std::pair<std::size_t, std::size_t>> q;
+    q.push(goal_cell);
+
+    // Execute the brushfire algorithm to assign distances to each cell
+    while(!q.empty()) {
+        std::pair<std::size_t, std::size_t> cell = q.front();
+        q.pop();
+
+        // Populate the neighbors distances
+        if(cell.first > 0 && distanceArr[cell.first-1][cell.second] == 0) {
+            distanceArr[cell.first-1][cell.second] = distanceArr[cell.first][cell.second] + 1;
+            q.push({cell.first-1, cell.second});
+        }
+        if(cell.first < n_cells.first - 1 && distanceArr[cell.first+1][cell.second] == 0) {
+            distanceArr[cell.first+1][cell.second] = distanceArr[cell.first][cell.second] + 1;
+            q.push({cell.first+1, cell.second});
+        }
+        if(cell.second > 0 && distanceArr[cell.first][cell.second-1] == 0) {
+            distanceArr[cell.first][cell.second-1] = distanceArr[cell.first][cell.second] + 1;
+            q.push({cell.first, cell.second-1});
+        }
+        if(cell.second < n_cells.second - 1 && distanceArr[cell.first][cell.second+1] == 0) {
+            distanceArr[cell.first][cell.second+1] = distanceArr[cell.first][cell.second] + 1;
+            q.push({cell.first, cell.second+1});
+        }
+    }
+
+    // Get the initial cell
+    std::pair<std::size_t, std::size_t> init_cell = grid_cspace.getCellFromPoint(q_init(0), q_init(1));
+
+    // Get the path from the initial cell to the goal cell
+    std::pair<std::size_t, std::size_t> curr_cell = init_cell;
+    bool found_goal = false;
+
+    while(!found_goal) {
+        // Loop through all neigbors and push them onto a vector
+        std::vector<std::pair<std::size_t, std::size_t>> neighbors;
+        std::vector<double> distance;
+
+        // Add all neighbors to the vector
+        if(curr_cell.first > 0) {
+            neighbors.push_back({curr_cell.first-1, curr_cell.second});
+            distance.push_back(distanceArr[curr_cell.first-1][curr_cell.second]);
+        }
+        if(curr_cell.first < n_cells.first - 1) {
+            neighbors.push_back({curr_cell.first+1, curr_cell.second});
+            distance.push_back(distanceArr[curr_cell.first+1][curr_cell.second]);
+        }
+        if(curr_cell.second > 0) {
+            neighbors.push_back({curr_cell.first, curr_cell.second-1});
+            distance.push_back(distanceArr[curr_cell.first][curr_cell.second-1]);
+        }
+        if(curr_cell.second < n_cells.second - 1) {
+            neighbors.push_back({curr_cell.first, curr_cell.second+1});
+            distance.push_back(distanceArr[curr_cell.first][curr_cell.second+1]);
+        }
+
+
+    }
+
+
+
     if (isManipulator) {
         Eigen::Vector2d bounds0 = Eigen::Vector2d(0.0, 0.0);
         Eigen::Vector2d bounds1 = Eigen::Vector2d(2*M_PI, 2*M_PI);
